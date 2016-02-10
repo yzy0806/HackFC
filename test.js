84f4ffc87a5e3975f159abc74121f2d8dcfc734a
Leo Ying <yzy0806@gmail.com>
2016-02-10 01:18:16 -0500

    done

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 function getLinks() {
     var links = document.querySelectorAll('.subject .center-cell a[href]');
     return Array.prototype.map.call(links, function(e) {
-        return e.getAttribute('href');
+        if(e.getAttribute('href').search('amid')==-1){
+            return e.getAttribute('href');
+        }
     });
 };
+
+function getName(){
+    var name=document.querySelector('strong.profilehdg').innerHTML.slice(0,-5);
+    return name;
+}
 
 var casper = require('casper').create({
     verbose: true,
@@ -34,10 +41,6 @@ casper.start('https://www.filipinocupid.com/en/auth/login?timeout&page=/en/mail/
     this.thenOpen('http://www.filipinocupid.com/en/mail/showinbox/?orderBy=1&page=100', function() {
         lastPageUrl=this.getCurrentUrl().search('page=')+5;
         lastPage=parseInt(this.getCurrentUrl().substring(lastPageUrl));
-        console.log("hey");
-        console.log(lastPage);
-        console.log(lastPageUrl);
-        console.log("yo");
         casper.capture("../images/fc1.png");
     });
 
@@ -47,10 +50,30 @@ casper.start('https://www.filipinocupid.com/en/auth/login?timeout&page=/en/mail/
         this.thenOpen(url);
         this.then(function(){
             allGirlUrls = allGirlUrls.concat(this.evaluate(getLinks));
-            this.capture("../images/fc.png");
         });
     }
     });
+
+    casper.then(function(){
+        for(var i=0;i<allGirlUrls.length;i++){
+            var girlUrl ='http://www.filipinocupid.com'+allGirlUrls[i];
+            this.thenOpen(girlUrl);
+            this.then(function(){
+                var girlName=this.evaluate(getName);
+                var message= "Dear beautiful"+girlName+
+                "\n I really like you and would like to know you more. Would you like to add me on Skype?. My username is yingz12."+ 
+                "\n Thank you "+ "\nLeo "
+                this.echo(message);
+                this.echo(girlUrl);
+                this.fill('form#emailreplyform', {
+                    'subject':    'default_5',
+                    'body':    message
+                }, true);
+            })
+            this.capture("../images/fc.png");                
+        }
+    })
+
 });
 
 casper.run(function() {
